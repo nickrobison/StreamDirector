0ee46f33180ac0b57f3efa753f20d64b4d35c14a
Nick Robison <nick@nickrobison.com>
2025-11-26 13:35:49 -0800

    Add SQLiteData
    Add SQLite dependency, since SwiftData seems WAY too complicated.

---
+++ PTZOKit/PTZOKit/Data/Schema.swift
@@ -0,0 +1,28 @@
+//
+//  CameraRecord.swift
+//  PTZOKit
+//
+//  Created by Nick Robison on 11/26/25.
+//
+
+import Foundation
+import SQLiteData
+
+@Table
+struct CameraRecord: Hashable, Identifiable {
+    let id: UUID
+    var name: String
+    var hostname: String
+    var port: String
+    @Column(as: CameraConfiguration.JSONRepresentation.self)
+    var configuration: CameraConfiguration
+}
+
+@Table
+struct PresetRecord: Hashable, Identifiable {
+    let id: Int
+    var name = ""
+    var cameraId: CameraRecord.ID
+    @Column(as: PresetValue.JSONRepresentation.self)
+    var value: PresetValue
+}
